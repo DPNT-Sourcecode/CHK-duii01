b8a7d8031e3da2fca38df37be4455d12b20ad924
sbx_user1051 <sbx_user1051@169.254.109.157>
2024-09-16 15:34:29 +0000

    Mon Sep 16 15:34:29 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,20 +1,21 @@
 """
 Our price table and offers:
-+------+-------+----------------+
-| Item | Price | Special offers |
-+------+-------+----------------+
-| A    | 50    | 3A for 130     |
-| B    | 30    | 2B for 45      |
-| C    | 20    |                |
-| D    | 15    |                |
-+------+-------+----------------+
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
++------+-------+------------------------+
 """
 import math
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+    prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
     illegal_input = False
     for letter in skus:
         if letter not in prices.keys():
@@ -22,6 +23,9 @@ def checkout(skus):
             total = -1
     if illegal_input == False:
         discounts = {"A": (3,130), "B": (2,45)}
+        get_free = {"E": (2,(1,"B"))}
+        for key in get_free.keys():
+            
         letter_dict = {char: skus.count(char) for char in set(skus)}
         totals = {}
         for key in letter_dict.keys():
@@ -30,12 +34,12 @@ def checkout(skus):
                 discounts_applied = math.floor(letter_dict[key]/discounts[key][0])
                 totals[key] = (letter_dict[key] - discounts_applied * discounts[key][0]) * prices[key] + discounts_applied*discounts[key][1]
             else:
-                discounts_applied = 0
                 totals[key] = letter_dict[key] * prices[key]
         total = 0
         for value in totals.values():
             total += value
     return total
+
 
 
 
