7c49e3f4c17877f90c37d1c1d922c9696e1ab036
sbx_user1051 <sbx_user1051@169.254.109.157>
2024-09-16 16:35:01 +0000

    Mon Sep 16 16:35:01 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -24,29 +24,31 @@ def checkout(skus):
             break
     if illegal_input == False:
         get_free = {"E": (2, (1, "B"))}
-        letter_dict = {char: skus.count(char) for char in set(skus)}
+        letter_count = {char: skus.count(char) for char in set(skus)}
 
         for key in get_free:
-            if key in letter_dict:
-                free_discount_applied = math.floor(letter_dict[key]/get_free[key][0])
+            if key in letter_count:
+                free_discount_applied = math.floor(letter_count[key]/get_free[key][0])
                 reduction = free_discount_applied * get_free[key][1][0]
-                if get_free[key][1][1] in letter_dict.keys():
-                    letter_dict[get_free[key][1][1]] -= reduction
+                if get_free[key][1][1] in letter_count.keys():
+                    letter_count[get_free[key][1][1]] -= reduction
         totals = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
 
         discounts = {"A": [(5, 120), (3, 130)], "B": [(2, 45)]}
-        for key in letter_dict:
-            if letter_dict[key] < 0:
+        for key in letter_count:
+            if letter_count[key] < 0:
                 pass
             elif key in discounts:
                 for deal in discounts[key]:
-                    discounts_applied = math.floor(letter_dict[key]/deal[0])
-                    totals[key] += (letter_dict[key] - discounts_applied * deal[0]) * prices[key] + discounts_applied*deal[1]
-                    letter_dict[key] -= discounts_applied * deal[0]
+                    discounts_applied = math.floor(letter_count[key]/deal[0])
+                    letter_count[key] = letter_count[key] - discounts_applied * deal[0]
+                    totals[key] += discounts_applied*deal[1]
+                    print(letter_count)
+                totals[key] += letter_count[key] * prices[key]
             else:
-                totals[key] = letter_dict[key] * prices[key]
+                totals[key] = letter_count[key] * prices[key]
         total = sum(totals.values())
     return total
 
 
-print(checkout("AAAAABAAA"))
+print(checkout("AAAAABAAA")) # -> 360
